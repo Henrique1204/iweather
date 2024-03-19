f92d0addc33ec0ee0e9b37cbb668f8abe1bfbd33
Paulo <pauloh16.pdr@gmail.com>
2024-03-19 01:53:02 -0300

    Criar testes para servicos

---
+++ src/services/getCityByNameService.test.ts
@@ -0,0 +1,15 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
+
+import { api } from "./api";
+
+import { getCityByNameService } from "./getCityByNameService";
+
+describe("API: getCityByNameService", () => {
+  it("Should return city details.", async () => {
+    jest.spyOn(api, "get").mockResolvedValue({ data: mockCityAPIResponse });
+
+    const response = await getCityByNameService(mockCityAPIResponse.name);
+
+    expect(response.length).toBeGreaterThan(0);
+  });
+});
